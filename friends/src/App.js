4436ba0a1ff76356148d034671be6ce69d61ead9
RyanNFreeman <ryanfreeman@Ryans-iMac.local>
2019-02-28 21:24:21 -0700

    sending what I have so far. Need to revisit.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+// import { Route, Link } from 'react-router-dom'
 import Friends from './components/Friends'
+import FriendForm from './components/FriendForm'
 import './App.css';
+import axios from 'axios';
 
 const style={
-  'fontSize': '75px',
-  'textAlign': 'center'
+  'fontSize': '55px',
+  'textAlign': 'center',
+  'margin': '5px'
 }
 const button={
   'margin': '50px',
@@ -19,28 +23,48 @@ const button={
 class App extends Component {
   constructor(){
     super();
+    this.state = {
+      friends: []
+    }
   }
 
+  addFriend = (e, friends) => {
+    e.preventDefault();
+    axios.post('http://localhost:5000/friends', friends)
+    .then(res => {
+        this.setState({friends: res.data});
+        console.log(res)
+  })
+  .catch(err => console.log(err + 'wrong'));
+
+}
+
+
+  componentDidMount(){
+    axios.get('http://localhost:5000/friends')
+    .then(res => this.setState({friends: res.data}))
+    .catch(err => console.log(err + 'wrong'))
+  }
+
+
   render() {
-    
 
     return (
       <div className="App">
         <header className="App-header">
           <p style={style}>
             LOOK AT YOU MR. POPULAR
-            <hr/>
           </p>
-
-          <input
+          {/* <input
             className="App-input"
             type="text"
             value="Add more friends..."
             style={style}
-          />
+          /> */}
           <Friends />
-          {/* <FriendForm /> */}
-          <button style={button}>Add a new friend!</button>
+          <FriendForm addFriend={this.addFriend}/>
+          {/* <Link to='/new-friend'><button style={button}>Add a new friend!</button></Link>
+          <Route exact path='/new-friend' component={FriendForm}/> */}
         </header>
       </div>
     );
--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -1,21 +1,55 @@
-// import React from 'react'
+import React from 'react'
 
-// const FriendForm = () => {
-//     return (
-//         <div>
-//             <form onChange={onChange}>
-//                 <input>
-//                 name
-//                 </input>
-//                 <input>
-//                 age
-//                 </input>
-//                 <input>
-//                 email
-//                 </input>
-//             </form>
-//         </div>
-//     )
-// }
+class FriendForm extends React.Component {
+    constructor() {
+        super();
+        this.state={
+            friend: {
+                name: '',
+                age: '',
+                email: ''
+            }
+        }
+    }
 
-// export default FriendForm;
+    changeHandle = (e) => {
+        this.setState({
+            friend: {
+            [e.target.name]: e.target.value}
+        })
+    }
+
+    render() {
+    return (
+        <div>
+            <h2>Would you like to have more friends?</h2>
+            <form>
+                <input 
+                    name='name' 
+                    type='text' 
+                    onChange={this.changeHandle} 
+                    placeholder='Name' 
+                    value={this.state.friend.name} 
+                    />
+                <input 
+                    name='age' 
+                    type='text' 
+                    onChange={this.changeHandle} 
+                    placeholder='Age' 
+                    value={this.state.friend.age} 
+                    />
+                <input 
+                    name='email' 
+                    type='text' 
+                    onChange={this.changeHandle} 
+                    placeholder='Email'
+                    value={this.state.friend.email} 
+                    />
+                <button onClick={e => this.props.addFriend(e, this.state.friend)}>Add Friend</button>
+            </form>
+        </div>
+        )
+    }
+}
+
+export default FriendForm;
